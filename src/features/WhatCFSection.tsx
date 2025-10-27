822bc241ca1233c4a38a47892f4ccaeae8851dfe
japanese-knife <japanese.traditional.knife@gmail.com>
2025-10-27 10:16:15 +0900

    Updated WhatCFSection.tsx

--- src/features/WhatCFSection.tsx
+++ src/features/WhatCFSection.tsx
@@ -7,8 +7,6 @@ import styles from "./WhatCFSection.module.css";
 export default function WhatCFSection({ isMobile }: { isMobile?: boolean }) {
 
 
-  const title = isMobile === undefined || isMobile === true ? "海外クラファンとは" : "海外クラウドファンディングとは";
-  const subTitle = "あらかじめ売れる数を把握してからの海外進出\n\n";
   const description = isMobile === undefined || isMobile === true
     ? "海外クラウドファンディングでは、在庫を抱える心配がなく、低リスクで受注生産型の販売が可能です。また、実際の海外市場でテストマーケティングを行い、販路を拡大することもできます。"
     : "海外クラウドファンディングでは、在庫を抱える心配がなく、低リスクで受注生産型の販売が可能です。また、実際の海外市場でテストマーケティングを行い、販路を拡大することもできます。\n弊社では、日本の事業者様に対して、この海外クラウドファンディングへの挑戦に関わる全てを一括でサポートさせていただいております。";
