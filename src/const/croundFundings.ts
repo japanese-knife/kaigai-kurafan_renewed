ad73316539932a9cf64024b8869a56c78ebb2cc1
japanese-knife <japanese.traditional.knife@gmail.com>
2026-01-31 13:37:48 +0900

    Updated croundFundings.ts

--- src/const/croundFundings.ts
+++ src/const/croundFundings.ts
@@ -89,7 +89,7 @@ export const CROUD_FUNDINGS_DATA: DataObjectType = {
   // Karatto Plus
   ybb: {
     userName: "カラッとプラス（ブランド）",
-    userImgSrc: "KARATTO_PLUS.png",
+    userImgSrc: "karatto_plus.png",
     tag: "シューケア / デバイス",
     title: "Karatto Plus | 次世代シューズ用除湿＆消臭デバイス",
     description:
